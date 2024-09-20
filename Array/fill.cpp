9b4d000ec8dc6bc15e640457d01142275aed966a
Puneet Tiwari <tiwaripuneet426@gmail.com>
2024-09-20 16:14:40 +0530

    fill the array

---
+++ Array/fill.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[4];
+    fill(arr,arr+4,30);
+    cout<< arr[0] << " "<<arr[1] << " "<<arr[2] << " "<<arr[3] <<endl;
+    return 0;
+}
